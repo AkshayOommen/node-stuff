35788cfc0d3928b0fad8758e7f15f6665bd8c5eb
Akshay Oommen <axeabraham@hotmail.com>
2017-08-25 11:10:35 +0530

    Added a custom emitter module

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 var greet = require('./greet');
+var Emitter = require('./emitter');
+
+var emtr = new Emitter();
+
+emtr.on('log', function () {
+    console.log('A log has been displayed on the console.');
+});
+
+emtr.on('log', function () {
+    console.log('A log has been displayed on the console!!');
+});
 
 console.log(greet.english.props.languageName);
+emtr.emit('log');
 console.log(greet.spanish.languageName);
+emtr.emit('log');
---
+++ emitter/index.js
@@ -0,0 +1,3 @@
+var emitter = require('./emitter');
+
+module.exports = emitter;
