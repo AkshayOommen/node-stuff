9b1e41ebaa24795d1658667a7bfde3eeb43e4cf6
Akshay Oommen <axeabraham@hotmail.com>
2017-08-26 13:24:23 +0530

    Playing around with buffers

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
-var greet = require('./greet');
-var Emitter = require('./emitter');
-var Person = require('./Person');
+// var greet = require('./greet');
+// var Emitter = require('./emitter');
+// var Person = require('./Person');
 var Util = require('util');
-var Events = require('events');
+// var Events = require('events');
 
 
 // var emtr = new Emitter();
 
 // This extends the prototype of Person constructor with the Events Prototype
 
-Util.inherits(Person, Events)
+// Util.inherits(Person, Events)
 
-var emtr = new Person('John', 'Doe', 'Mr');
+// var emtr = new Person('John', 'Doe', 'Mr');
 
-emtr.on('log', function (data) {
-    console.log(`A log has been displayed on the console for ${data.displayName()}`);
-});
+// emtr.on('log', function (data) {
+//     console.log(`A log has been displayed on the console for ${data.displayName()}`);
+// });
 
-console.log(greet.english.props.languageName);
-emtr.emit('log', emtr);
-console.log(greet.spanish.languageName);
+// console.log(greet.english.props.languageName);
+// emtr.emit('log', emtr);
+// console.log(greet.spanish.languageName);
+
+// function Book () {
+//     this.title = 'Catch 22',
+//     this.author = 'Joseph Keller',
+//     this.displayBookInfo = function () {
+//         console.log(`The book name is ${this.title} and it is written by ${this.author}`);
+//     }
+// };
+
+// Book.prototype.displayBookInfo = function () {
+//     console.log(`The book name is ${this.title} and it is written by ${this.author}`);
+// }
+
+// var book = new Book();
+// book.displayBookInfo();
+
+// function FictionalBook () {
+//     this.genre = 'Humor'
+// }
+
+// Util.inherits(FictionalBook, Book);
+// var fictBook = new FictionalBook();
+// fictBook.displayBookInfo();
+
+var buffer = new Buffer('Thomas');
+console.log(buffer);
+console.log(buffer.toJSON());
+console.log(buffer.toString());
+console.log(buffer[2]);
+
+buffer.write('Axe');
+console.log(buffer.toString());
