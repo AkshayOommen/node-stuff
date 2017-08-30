382ffd9c02547ed584ab833046298102f037988c
Akshay Oommen <axeabraham@hotmail.com>
2017-08-30 19:36:44 +0530

    Sending back text files using fileSync, streams + sending back JSONs using HTTP

--- server.js
+++ server.js
@@ -7,7 +7,30 @@ http.createServer(function(req,res) {
     // res.end('Welcome to NodeJS! \n\nEnjoy your stay here. :)');
 
     // Returning a text file
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var txt = fs.readFileSync(__dirname + '/samples/sampleA.txt');
-    res.end(txt);
+    // res.writeHead(200, {'Content-Type': 'text/plain'});
+    // var txt = fs.readFileSync(__dirname + '/samples/sampleA.txt', 'utf8');
+    // txt = txt.replace('{Title}', 'Lorem Ipsum');
+    // res.end(txt);
+
+    // Returning a text file using streams
+    // res.writeHead(200, {'Content-Type': 'text/plain'});
+    // fs.createReadStream(__dirname + '/samples/sampleA.txt').pipe(res);
+    
+    // Returning a JSON
+    res.writeHead(200, {'Content-Type': 'application/json'});
+
+    var list = {
+        employees: [{
+            id: 1,
+            name: 'Akshay Oommen'
+        }, {
+            id: 2,
+            name: 'Tina Thomas'
+        }, {
+            id: 3,
+            name: 'Archie Andrews'
+        }]
+    };
+
+    res.end(JSON.stringify(list));
 }).listen(8085, '127.0.0.1');
