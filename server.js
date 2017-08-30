c8494dd826c13c87d7ba499fe95ce35b9dd317f5
Akshay Oommen <axeabraham@hotmail.com>
2017-08-30 19:05:08 +0530

    Returning a text file using http

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(req,res) {
+    // Returning a text message
+    // res.writeHead(200, {'Content-Type': 'text/plain'});
+    // res.end('Welcome to NodeJS! \n\nEnjoy your stay here. :)');
+
+    // Returning a text file
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Welcome to NodeJS! \n\nEnjoy your stay here. :)');
+    var txt = fs.readFileSync(__dirname + '/samples/sampleA.txt');
+    res.end(txt);
 }).listen(8085, '127.0.0.1');
