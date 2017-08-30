9b8f71bde6af48b5105ef905415ef964cc4eee67
Akshay Oommen <axeabraham@hotmail.com>
2017-08-30 20:52:20 +0530

    Routing using nodeJS

--- server.js
+++ server.js
@@ -15,22 +15,60 @@ http.createServer(function(req,res) {
     // Returning a text file using streams
     // res.writeHead(200, {'Content-Type': 'text/plain'});
     // fs.createReadStream(__dirname + '/samples/sampleA.txt').pipe(res);
-    
+
     // Returning a JSON
-    res.writeHead(200, {'Content-Type': 'application/json'});
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    // var list = {
+    //     employees: [{
+    //         id: 1,
+    //         name: 'Akshay Oommen'
+    //     }, {
+    //         id: 2,
+    //         name: 'Tina Thomas'
+    //     }, {
+    //         id: 3,
+    //         name: 'Archie Andrews'
+    //     }]
+    // };
+    // res.end(JSON.stringify(list));
 
-    var list = {
-        employees: [{
-            id: 1,
-            name: 'Akshay Oommen'
-        }, {
-            id: 2,
-            name: 'Tina Thomas'
-        }, {
-            id: 3,
-            name: 'Archie Andrews'
-        }]
-    };
+    // Basic routing using nodejs
+    if (req.url === '/employee-json') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var list = {
+            employees: [{
+                id: 1,
+                name: 'Akshay Oommen'
+            }, {
+                id: 2,
+                name: 'Tina Thomas'
+            }, {
+                id: 3,
+                name: 'Archie Andrews'
+            }]
+        };
+        res.end(JSON.stringify(list)); 
+    } else if (req.url === '/student-json') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var list = {
+            students: [{
+                id: 1,
+                name: 'James Cormen'
+            }, {
+                id: 2,
+                name: 'Jimmy Fallon'
+            }, {
+                id: 3,
+                name: 'Dave Meltzer'
+            }]
+        };
+        res.end(JSON.stringify(list)); 
 
-    res.end(JSON.stringify(list));
+    } else if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        fs.createReadStream(__dirname + '/samples/sampleA.txt').pipe(res);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('');
+    }
 }).listen(8085, '127.0.0.1');
