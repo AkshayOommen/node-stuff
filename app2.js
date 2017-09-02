00b6e1ec7e488db6a5d1e48a643b0bdcb0fa534a
Akshay Oommen <axeabraham@hotmail.com>
2017-09-02 15:48:01 +0530

    Parameterized routing

--- app2.js
+++ app2.js
@@ -11,4 +11,15 @@ app.get('/sample', function(req,res) {
     res.json({firstName: 'Akshay', lastName: 'Oommen'});
 })
 
+app.get('/person/:id', function(req,res) {
+    var employees = {
+        1: {name: 'Akshay Oommen', designation: 'Senior Software Developer'},
+        2: {name: 'Vaibhav Singh', designation: 'Software Developer'},
+        3: {name: 'Piyush Das', designation: 'Senior Software Developer'},
+    }
+
+    res.send('<html><head><h1>Welcome to Express!</h1></head><body>' 
+    + employees[req.params.id].name + ' - ' + employees[req.params.id].designation + '</body></html>')
+})
+
 app.listen(port);
