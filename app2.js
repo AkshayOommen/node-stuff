0b0ed672024ce66559a85cb6ea3ac6252060716c
Akshay Oommen <axeabraham@hotmail.com>
2017-09-02 16:32:59 +0530

    Serving static files in middleware using express

--- app2.js
+++ app2.js
@@ -3,8 +3,15 @@ var app = express();
 
 var port = process.env.port || 3000;
 
+app.use('/person/assets', express.static(__dirname + '/public'));
+
+app.use('/', function(req, res, next) {
+    console.log(req.url);   
+    next();
+});
+
 app.get('/', function(req,res) {
-    res.send('<html><head><h1>Welcome to Express!</h1></head><body>Enjoy your stay here.</body></html>')
+    res.send('<html><head></head><body><h1>Welcome to Express!</h1><p>Enjoy your stay here.</p></body></html>')
 });
 
 app.get('/sample', function(req,res) {
@@ -18,8 +25,9 @@ app.get('/person/:id', function(req,res) {
         3: {name: 'Piyush Das', designation: 'Senior Software Developer'},
     }
 
-    res.send('<html><head><h1>Welcome to Express!</h1></head><body>' 
-    + employees[req.params.id].name + ' - ' + employees[req.params.id].designation + '</body></html>')
+    res.send('<html><head><link href="assets/style.css" type="text/css" rel="stylesheet"/>\
+    </head><body><h1>Welcome to Express!</h1><p>\
+    ' + employees[req.params.id].name + ' - ' + employees[req.params.id].designation + '</p></body></html>')
 })
 
 app.listen(port);
