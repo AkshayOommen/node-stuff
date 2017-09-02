a6355c82d3836e7ed26644a676056cdb594c9d47
Akshay Oommen <axeabraham@hotmail.com>
2017-09-02 17:19:56 +0530

    Added ejs templating engine for dynamic html

--- app2.js
+++ app2.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 var port = process.env.port || 3000;
 
-app.use('/person/assets', express.static(__dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
 
 app.use('/', function(req, res, next) {
     console.log(req.url);   
@@ -25,9 +29,7 @@ app.get('/person/:id', function(req,res) {
         3: {name: 'Piyush Das', designation: 'Senior Software Developer'},
     }
 
-    res.send('<html><head><link href="assets/style.css" type="text/css" rel="stylesheet"/>\
-    </head><body><h1>Welcome to Express!</h1><p>\
-    ' + employees[req.params.id].name + ' - ' + employees[req.params.id].designation + '</p></body></html>')
+    res.render('person', {emp: employees[req.params.id]});
 })
 
 app.listen(port);
