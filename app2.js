1ee8fb11b610cbd844915b6763e06470674f2022
Akshay Oommen <axeabraham@hotmail.com>
2017-09-03 23:29:33 +0530

    Using different parsers to process query parameters in GET and POST calls

--- app2.js
+++ app2.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
+
+var urlEncodedParser = bodyParser.urlencoded({excluded: false});
+var jsonParser = bodyParser.json();
 
 var port = process.env.port || 3000;
 
 app.use('/assets', express.static(__dirname + '/public'));
+app.use('/lib', express.static(__dirname + '/bower_components'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -20,7 +25,7 @@ app.get('/', function(req,res) {
 
 app.get('/sample', function(req,res) {
     res.json({firstName: 'Akshay', lastName: 'Oommen'});
-})
+});
 
 app.get('/person/:id', function(req,res) {
     var employees = {
@@ -30,6 +35,27 @@ app.get('/person/:id', function(req,res) {
     }
 
     res.render('person', {emp: employees[req.params.id]});
-})
+});
+
+// Parsing the query parameters using the 'query' object
+app.get('/animal', function(req,res) {
+    res.send('Returning a query string');
+    console.log(req.query.firstName);
+    console.log(req.query.lastName);
+});
+
+// Parsing the query parameters using the urlEncodedParser
+app.post('/animal-url-encoded', urlEncodedParser, function(req,res) {
+    res.send('Returning a query string using urlEncodedParser');
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
+});
+
+// Parsing the query parameters using the jsonParser
+app.post('/animal-json', jsonParser, function(req,res) {
+    res.send('Returning a query string using jsonParser');
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
+});
 
 app.listen(port);
